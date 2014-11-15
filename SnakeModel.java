b8c9a170dfd28129e1937d1acf7f062989be2152
Tbbas <covernant_4@hotmail.com>
2014-11-15 13:50:05 +0100

    SnakeModel
    nästan klar

--- SnakeModel.java
+++ SnakeModel.java
@@ -4,7 +4,6 @@ import java.awt.Dimension;
 import java.awt.event.KeyEvent;
 import java.lang.ProcessBuilder.Redirect;
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * A simple "Snake"-Game.
@@ -54,14 +53,13 @@ public class SnakeModel extends GameModel {
 			0),
 			new Color(255, 0, 0), 2.0);
 
-	/** Graphical representation of the collector */
-	private static final GameTile SNAKE_TILE = new RoundTile(Color.BLACK,
-			Color.RED, 2.0);
+	/** Graphical representation of the snake */
+	private static final GameTile SNAKE_TILE = new RectangularTile(Color.BLACK);
 
 	/** Graphical representation of a blank tile. */
 	private static final GameTile BLANK_TILE = new GameTile();
 
-	/** THe position of the apple */
+	/** The position of the apple */
 	private Position appleLocation;
 	/*
 	 * The declaration and object creation above uses the new language feature
@@ -72,24 +70,26 @@ public class SnakeModel extends GameModel {
 	 * to be used in the List
 	 */
 
-	/** The position of the collector. */
-	private Position SnakePos;
+	/** Length of the snake */
 
-	/** The direction of the collector. */
+	private int snakeLength;
+
+	/** A list of the snake's positions in the board. */
+	private ArrayList<Position> SnakePos = new ArrayList<Position>();
+
+	/** The direction of the snake. */
 	private Directions direction = Directions.NORTH;
 
-	/** The number of coins found. */
+	/** Number of apples eaten */
 	private int score;
-	
-	private boolean addApple;
-	
+
 
 	/**
-	 * Create a new model for the gold game.
+	 * Constructor, create a new SnakeModel-game. 
 	 */
 	public SnakeModel() {
+		snakeLength = 3;
 		Dimension size = getGameboardSize();
-		addApple = true;
 
 		// Blank out the whole gameboard
 		for (int i = 0; i < size.width; i++) {
@@ -98,36 +98,35 @@ public class SnakeModel extends GameModel {
 			}
 		}
 
-		// Insert the collector in the middle of the gameboard.
-		this.SnakePos = new Position(size.width / 2, size.height / 2);
-		setGameboardState(this.SnakePos, SNAKE_TILE);
+		// Insert the snake in the middle of the gameboard.
+		for(int i = 0; i < snakeLength; i++) {
+			this.SnakePos.add(new Position(size.width / 2, (size.height / 2) + i ));
+			setGameboardState(this.SnakePos.get(i), SNAKE_TILE);
+		}
 
-		//Insert apple to the gameboard.
-			addCoin();
+		//Insert the first apple to the gameboard.
+		addApple();
 	}
 
 	/**
 	 * Insert an apple at a random location
 	 */
-	private void addCoin() {
-		Position newCoinPos;
+	private void addApple() {
+		Position newApplePos;
 		Dimension size = getGameboardSize();
 		// Loop until a blank position is found and ...
-		if(addApple) {
 		do {
-			newCoinPos = new Position((int) (Math.random() * size.width),
-										(int) (Math.random() * size.height));
-		} while (!isPositionEmpty(newCoinPos));
+			newApplePos = new Position((int) (Math.random() * size.width),
+					(int) (Math.random() * size.height));
+		} while (!isPositionEmpty(newApplePos));
 
-		// ... add a new coin to the empty tile.
-		setGameboardState(newCoinPos, APPLE_TILE);
-		this.appleLocation = newCoinPos;
-		addApple = false;
-		}
+		// ... add an apple to the empty tile.
+		setGameboardState(newApplePos, APPLE_TILE);
+		this.appleLocation = newApplePos;
 	}
 
 	/**
-	 * Return whether the specified position is empty.
+	 * Return whether the specified position is empty or a snake-tile.
 	 * 
 	 * @param pos
 	 *            The position to test.
@@ -143,32 +142,37 @@ public class SnakeModel extends GameModel {
 	 */
 	private void updateDirection(final int key) {
 		switch (key) {
-			case KeyEvent.VK_LEFT:
-				this.direction = Directions.WEST;
-				break;
-			case KeyEvent.VK_UP:
-				this.direction = Directions.NORTH;
-				break;
-			case KeyEvent.VK_RIGHT:
-				this.direction = Directions.EAST;
-				break;
-			case KeyEvent.VK_DOWN:
-				this.direction = Directions.SOUTH;
-				break;
-			default:
-				// Don't change direction if another key is pressed
-				break;
+		case KeyEvent.VK_LEFT:
+			this.direction = Directions.WEST;
+			break;
+		case KeyEvent.VK_UP:
+			this.direction = Directions.NORTH;
+			break;
+		case KeyEvent.VK_RIGHT:
+			this.direction = Directions.EAST;
+			break;
+		case KeyEvent.VK_DOWN:
+			this.direction = Directions.SOUTH;
+			break;
+		default:
+			// Don't change direction if another key is pressed
+			break;
 		}
 	}
 
 	/**
-	 * Get next position of the collector.
-	 */
-	private Position getNextCollectorPos() {
+	 * Get next position of the snake.
+	 */	
+	private Position getNextSnakePos() {
 		return new Position(
-				this.SnakePos.getX() + this.direction.getXDelta(),
-				this.SnakePos.getY() + this.direction.getYDelta());
+				SnakePos.get(0).getX() + direction.getXDelta(),
+				SnakePos.get(0).getY() + direction.getYDelta());
+
+
+
 	}
+
+
 
 	/**
 	 * This method is called repeatedly so that the
@@ -182,22 +186,29 @@ public class SnakeModel extends GameModel {
 		updateDirection(lastKey);
 
 		// Erase the previous position.
-		setGameboardState(this.SnakePos, BLANK_TILE);
-		// Change collector position.
-		this.SnakePos = getNextCollectorPos();
+		setGameboardState(this.SnakePos.get(snakeLength-1), BLANK_TILE);
+		// Change snake position.
+		SnakePos.add(0, getNextSnakePos());
 
-		if (isOutOfBounds(this.SnakePos)) {
+		if (isOutOfBounds(this.SnakePos.get(0))) {
 			throw new GameOverException(this.score);
 		}
-		// Draw collector at new position.
-		setGameboardState(this.SnakePos, SNAKE_TILE);
+		if (isSnakeTile(getNextSnakePos())) {
+			throw new GameOverException(this.score);
+		}
 
-		// Remove the coin at the new collector position (if any)
-		if (this.SnakePos.equals(this.appleLocation)) {
-			System.out.println("nom");
+		// If the snakes head is at the same location as the apple, 
+		// the snake consumes the apple, becomes one joint longer and another apple is placed on the board.
+		if (this.SnakePos.get(0).equals(this.appleLocation)) {
 			this.score++;
 			Position oldCoinPos = this.appleLocation;
 			setGameboardState(oldCoinPos, BLANK_TILE);
+			snakeLength++;
+			addApple();
+		}
+		// Draw the snake on the gameboard
+		for(int i = 0; i<snakeLength; i++) {
+			setGameboardState(this.SnakePos.get(i), SNAKE_TILE);
 		}
 
 		// TODO add win-clause
@@ -205,11 +216,22 @@ public class SnakeModel extends GameModel {
 			throw new GameOverException(this.score + 5);
 		}
 
-		// Remove one of the coins
-		
-		addCoin();
-		
 
+	}
+
+
+	/**
+	 * 
+	 * @param pos The position to test.
+	 *@return <code>True</code> if the position is a snake tile, <code>false</code> otherwise.
+	 */
+	private boolean isSnakeTile(Position pos) {
+		if(isOutOfBounds(pos)) { 
+			return false;
+		}
+		else {
+			return(getGameboardState(pos.getX(), pos.getY()) == SNAKE_TILE);
+		}
 	}
 
 	/**
