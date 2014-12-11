6f368931c8da198fe3578e0d03354cf39ba4b60d
Tbbas <covernant_4@hotmail.com>
2014-12-11 18:14:44 +0100

    Ver 3, kan ej fortsätta utan ditt :)

---
+++ Labb3/orig2011/v3/BlankTile.java
@@ -0,0 +1,14 @@
+package orig2011.v3;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+public class BlankTile implements GameTile {
+
+	@Override
+	public void draw(Graphics g, int x, int y, Dimension d) {
+		//Empty method, since this tile should not be drawn.
+		
+	}
+
+}
